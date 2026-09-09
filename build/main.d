98616e718fd8caba194d0bc971941705effb8faf
hadley557 <189adriel@gmail.com>
2026-09-08 20:56:32 -0700

    the change
    good

---
+++ build/main.d
@@ -0,0 +1,88 @@
+main.o: /project/source/main.cpp /opt/devkitpro/wups/include/wups.h \
+ /opt/devkitpro/wups/include/wups/common.h \
+ /opt/devkitpro/wups/include/wups/config.h \
+ /opt/devkitpro/wut/include/padscore/kpad.h \
+ /opt/devkitpro/wut/include/wut.h \
+ /opt/devkitpro/wut/include/wut_structsize.h \
+ /opt/devkitpro/wut/include/wut_types.h \
+ /opt/devkitpro/wut/include/wut_rplwrap.h \
+ /opt/devkitpro/wut/include/padscore/wpad.h \
+ /opt/devkitpro/wut/include/coreinit/time.h \
+ /opt/devkitpro/wut/include/coreinit/systeminfo.h \
+ /opt/devkitpro/wut/include/vpad/input.h \
+ /opt/devkitpro/wut/include/vpadbase/base.h \
+ /opt/devkitpro/wups/include/wups/config_api.h \
+ /opt/devkitpro/wups/include/wups/config/WUPSConfigCategory.h \
+ /opt/devkitpro/wups/include/wups/config/WUPSConfigItem.h \
+ /opt/devkitpro/wups/include/wups/function_patching.h \
+ /opt/devkitpro/wups/include/wups/hooks.h \
+ /opt/devkitpro/wups/include/wups/button_combo_internal.h \
+ /opt/devkitpro/wups/include/wups/button_combo/defines.h \
+ /opt/devkitpro/wups/include/wups/reent_internal.h \
+ /opt/devkitpro/wups/include/wups/storage.h \
+ /opt/devkitpro/wups/include/wups/meta.h \
+ /opt/devkitpro/wups/include/wups/wups_debug.h \
+ /opt/devkitpro/wut/usr/include/mocha/mocha.h \
+ /opt/devkitpro/wut/usr/include/mocha/commands.h \
+ /opt/devkitpro/wut/usr/include/mocha/otp.h \
+ /opt/devkitpro/wut/include/coreinit/filesystem.h \
+ /opt/devkitpro/wut/include/coreinit/alarm.h \
+ /opt/devkitpro/wut/include/coreinit/context.h \
+ /opt/devkitpro/wut/include/coreinit/threadqueue.h \
+ /opt/devkitpro/wut/include/coreinit/fastmutex.h \
+ /opt/devkitpro/wut/include/coreinit/ios.h \
+ /opt/devkitpro/wut/include/coreinit/messagequeue.h \
+ /opt/devkitpro/wut/include/coreinit/filesystem_fsa.h \
+ /opt/devkitpro/wut/include/sysapp/args.h \
+ /opt/devkitpro/wut/include/coreinit/mcp.h \
+ /opt/devkitpro/wut/include/coreinit/debug.h \
+ /opt/devkitpro/wut/include/coreinit/thread.h \
+ /opt/devkitpro/wut/include/coreinit/exception.h \
+ /opt/devkitpro/wut/include/coreinit/launch.h \
+ /opt/devkitpro/wut/include/sysapp/launch.h \
+ /opt/devkitpro/wut/include/nn/ffl/miidata.h \
+ /opt/devkitpro/wut/include/sysapp/switch.h
+/opt/devkitpro/wups/include/wups.h:
+/opt/devkitpro/wups/include/wups/common.h:
+/opt/devkitpro/wups/include/wups/config.h:
+/opt/devkitpro/wut/include/padscore/kpad.h:
+/opt/devkitpro/wut/include/wut.h:
+/opt/devkitpro/wut/include/wut_structsize.h:
+/opt/devkitpro/wut/include/wut_types.h:
+/opt/devkitpro/wut/include/wut_rplwrap.h:
+/opt/devkitpro/wut/include/padscore/wpad.h:
+/opt/devkitpro/wut/include/coreinit/time.h:
+/opt/devkitpro/wut/include/coreinit/systeminfo.h:
+/opt/devkitpro/wut/include/vpad/input.h:
+/opt/devkitpro/wut/include/vpadbase/base.h:
+/opt/devkitpro/wups/include/wups/config_api.h:
+/opt/devkitpro/wups/include/wups/config/WUPSConfigCategory.h:
+/opt/devkitpro/wups/include/wups/config/WUPSConfigItem.h:
+/opt/devkitpro/wups/include/wups/function_patching.h:
+/opt/devkitpro/wups/include/wups/hooks.h:
+/opt/devkitpro/wups/include/wups/button_combo_internal.h:
+/opt/devkitpro/wups/include/wups/button_combo/defines.h:
+/opt/devkitpro/wups/include/wups/reent_internal.h:
+/opt/devkitpro/wups/include/wups/storage.h:
+/opt/devkitpro/wups/include/wups/meta.h:
+/opt/devkitpro/wups/include/wups/wups_debug.h:
+/opt/devkitpro/wut/usr/include/mocha/mocha.h:
+/opt/devkitpro/wut/usr/include/mocha/commands.h:
+/opt/devkitpro/wut/usr/include/mocha/otp.h:
+/opt/devkitpro/wut/include/coreinit/filesystem.h:
+/opt/devkitpro/wut/include/coreinit/alarm.h:
+/opt/devkitpro/wut/include/coreinit/context.h:
+/opt/devkitpro/wut/include/coreinit/threadqueue.h:
+/opt/devkitpro/wut/include/coreinit/fastmutex.h:
+/opt/devkitpro/wut/include/coreinit/ios.h:
+/opt/devkitpro/wut/include/coreinit/messagequeue.h:
+/opt/devkitpro/wut/include/coreinit/filesystem_fsa.h:
+/opt/devkitpro/wut/include/sysapp/args.h:
+/opt/devkitpro/wut/include/coreinit/mcp.h:
+/opt/devkitpro/wut/include/coreinit/debug.h:
+/opt/devkitpro/wut/include/coreinit/thread.h:
+/opt/devkitpro/wut/include/coreinit/exception.h:
+/opt/devkitpro/wut/include/coreinit/launch.h:
+/opt/devkitpro/wut/include/sysapp/launch.h:
+/opt/devkitpro/wut/include/nn/ffl/miidata.h:
+/opt/devkitpro/wut/include/sysapp/switch.h:
